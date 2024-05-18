91598bee82541a5f506712f8d924af846cd01272
sarthak13 <sarthak.sachdev@mai.io>
2024-05-18 18:18:37 +0530

    added cors config

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 let youtubeId = "";
 
 app.use(express.json())
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // const allowedOrigins = ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
+    // const origin = req.headers.origin;
+    // if (allowedOrigins.includes(origin)) {
+    //      res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 app.get('/', (req,res) => {
     res.setHeader('content-type', 'text/json');
