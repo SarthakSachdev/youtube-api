bac21ce55ae5b556ea97006796d9d73470301935
sarthak13 <sarthak.sachdev@mai.io>
2024-05-18 18:26:07 +0530

    test

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
 let youtubeId = "";
-
-app.use(express.json())
-app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,6 +15,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
   });
+app.use(express.json())
+
 
 app.get('/', (req,res) => {
     res.setHeader('content-type', 'text/json');
